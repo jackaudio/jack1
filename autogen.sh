38c43d4e0e98d36a6fb1f8a76ad40e002729307a
wingo <wingo@0c269be4-1314-0410-8aa9-9f06e86f4224>
2001-11-23 23:44:58 +0000

    * added cvsignore files * fixed build process for automake 1.5
    git-svn-id: svn+ssh://jackaudio.org/trunk/jack@39 0c269be4-1314-0410-8aa9-9f06e86f4224

--- autogen.sh
+++ autogen.sh
@@ -1,6 +1,26 @@
 #!/bin/sh
 
-libtoolize --force && aclocal $ACLOCAL_FLAGS && automake --add-missing --foreign && autoconf
+libtoolize --force || {
+    echo "libtool failed, exiting..."
+    exit 1
+}
+
+aclocal $ACLOCAL_FLAGS || {
+    echo "aclocal \$ACLOCAL_FLAGS where \$ACLOCAL_FLAGS= failed, exiting..."
+    exit 1
+}
+
+automake --add-missing --foreign || {
+    echo "automake --add-missing --foreign failed, exiting..."
+    exit 1
+}
+
+autoconf || {
+    echo "autoconf failed, exiting..."
+    exit 1
+}
+
+echo "now run ./configure."
 
 exit 0
 
