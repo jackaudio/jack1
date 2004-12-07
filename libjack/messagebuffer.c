692343db1a8225a1d8f3f23f27cf8d64040ba49b
joq <joq@0c269be4-1314-0410-8aa9-9f06e86f4224>
2004-12-07 18:23:07 +0000

    [0.99.24] thread-safe messagebuffer.c
    git-svn-id: svn+ssh://jackaudio.org/trunk/jack@825 0c269be4-1314-0410-8aa9-9f06e86f4224

--- libjack/messagebuffer.c
+++ libjack/messagebuffer.c
@@ -26,13 +26,15 @@
 
 #include <jack/messagebuffer.h>
 
-#define MB_BUFFERS	32		/* must be 2^n */
+#define MB_BUFFERS	64		/* must be 2^n */
 
 static char mb_buffers[MB_BUFFERS][MB_BUFFERSIZE];
 static unsigned int mb_initialized = 0;
 static unsigned int mb_inbuffer = 0;
 static unsigned int mb_outbuffer = 0;
 static pthread_t mb_writer_thread;
+static pthread_mutex_t mb_write_lock;
+static pthread_cond_t mb_ready_cond;
 
 static void
 mb_flush()
@@ -46,10 +48,14 @@ mb_flush()
 static void *
 mb_thread_func(void *arg)
 {
+	pthread_mutex_lock(&mb_write_lock);
+
 	while (mb_initialized) {
-		usleep(1000);
+		pthread_cond_wait(&mb_ready_cond, &mb_write_lock);
 		mb_flush();
 	}
+
+	pthread_mutex_unlock(&mb_write_lock);
 
 	return NULL;
 }
@@ -59,6 +65,9 @@ jack_messagebuffer_init ()
 {
 	if (mb_initialized)
 		return;
+
+	pthread_mutex_init(&mb_write_lock, NULL);
+	pthread_cond_init(&mb_ready_cond, NULL);
 
 	mb_initialized = 1;
 
@@ -72,16 +81,26 @@ jack_messagebuffer_exit ()
 	if (!mb_initialized)
 		return;
 
+	pthread_mutex_lock(&mb_write_lock);
 	mb_initialized = 0;
+	pthread_cond_signal(&mb_ready_cond);
+	pthread_mutex_unlock(&mb_write_lock);
 
 	pthread_join(mb_writer_thread, NULL);
 	mb_flush();
+
+	pthread_mutex_destroy(&mb_write_lock);
+	pthread_cond_destroy(&mb_ready_cond);
 }
 
 
 void 
 jack_messagebuffer_add (const char *msg)
 {
-	strncpy(mb_buffers[mb_inbuffer], msg, MB_BUFFERSIZE - 1);
-	mb_inbuffer = (mb_inbuffer + 1) & (MB_BUFFERS - 1);
+	if (pthread_mutex_trylock(&mb_write_lock) == 0) {
+		strncpy(mb_buffers[mb_inbuffer], msg, MB_BUFFERSIZE - 1);
+		mb_inbuffer = (mb_inbuffer + 1) & (MB_BUFFERS - 1);
+		pthread_cond_signal(&mb_ready_cond);
+		pthread_mutex_unlock(&mb_write_lock);
+	}
 }
