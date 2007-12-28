38099a7c4ecfd80de9dea5989ee169d4ce3d3636
pieterpalmers <pieterpalmers@0c269be4-1314-0410-8aa9-9f06e86f4224>
2007-12-28 21:34:40 +0000

    Adapted and applied jack_lsp filter patch by Ken Restivo.
    Original message:
    
    jack_lsp -c  is great for showing all connections, but I needed a way to to filter out just those that were connected to a particular 
    device.
    
    "grep -v" isn't sufficient when using "jack_lsp -c", because the connected devices aren't on the same line as what tey are connected 
    to. So, rather than make the shell script really complicated, or the output of jack_lsp really ugly, I patched jack_lsp to add a 
    "filter" feature. 
    
    I needed this so that I could write a shell script that does "jack_lsp -c alsa_pcm:playback_2" to get a list of everything that is 
    connected to that port, and then it can jack_disconnect those (or some of those) and then jack_connect them to another device.
    
    I doubt that anyone else would need this feature, but, just in case, here it is.
    
    -ken
    
    
    
    git-svn-id: svn+ssh://jackaudio.org/trunk/jack@1074 0c269be4-1314-0410-8aa9-9f06e86f4224

--- example-clients/lsp.c
+++ example-clients/lsp.c
@@ -21,8 +21,9 @@ void
 show_usage (void)
 {
 	show_version ();
-	fprintf (stderr, "\nUsage: %s [options]\n", my_name);
-	fprintf (stderr, "List active Jack ports, and optionally display extra information.\n\n");
+	fprintf (stderr, "\nUsage: %s [options] [filter string]\n", my_name);
+	fprintf (stderr, "List active Jack ports, and optionally display extra information.\n");
+	fprintf (stderr, "Optionally filter ports which match ALL strings provided after any options.\n\n");
 	fprintf (stderr, "Display options:\n");
 	fprintf (stderr, "        -A, --aliases         List aliases for each port\n");
 	fprintf (stderr, "        -c, --connections     List connections to/from each port\n");
@@ -42,7 +43,8 @@ main (int argc, char *argv[])
 	jack_client_t *client;
 	jack_status_t status;
 	const char **ports, **connections;
-	unsigned int i, j;
+	unsigned int i, j, k;
+	int skip_port;
 	int show_aliases = 0;
 	int show_con = 0;
 	int show_port_latency = 0;
@@ -128,6 +130,15 @@ main (int argc, char *argv[])
 	ports = jack_get_ports (client, NULL, NULL, 0);
 
 	for (i = 0; ports[i]; ++i) {
+		// skip over any that don't match ALL of the strings presented at command line
+		skip_port = 0;
+		for(k=optind; k < argc; k++){
+			if(strstr(ports[i], argv[k]) == NULL ){
+				skip_port = 1;
+			}
+		}
+		if(skip_port) continue;
+
 		printf ("%s\n", ports[i]);
 
 		jack_port_t *port = jack_port_by_name (client, ports[i]);
