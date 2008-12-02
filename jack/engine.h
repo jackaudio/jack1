f41f4fc241c35ab9024a736efb3fb99d768b4b66
paul <paul@0c269be4-1314-0410-8aa9-9f06e86f4224>
2008-12-02 11:36:38 +0000

    fix debug message when acquiring the "problem" lock
    git-svn-id: svn+ssh://jackaudio.org/trunk/jack@3153 0c269be4-1314-0410-8aa9-9f06e86f4224

--- jack/engine.h
+++ jack/engine.h
@@ -192,7 +192,7 @@ jack_client_internal_by_id (jack_engine_t *engine, jack_client_id_t id);
 #define jack_try_rdlock_graph(e) pthread_rwlock_tryrdlock (&e->client_lock)
 #define jack_unlock_graph(e) { DEBUG ("release graph lock"); if (pthread_rwlock_unlock (&e->client_lock)) abort(); }
 
-#define jack_lock_problems(e) { DEBUG ("release problem lock"); if (pthread_mutex_lock (&e->problem_lock)) abort(); }
+#define jack_lock_problems(e) { DEBUG ("acquiring problem lock"); if (pthread_mutex_lock (&e->problem_lock)) abort(); }
 #define jack_unlock_problems(e) { DEBUG ("release problem lock"); if (pthread_mutex_unlock (&e->problem_lock)) abort(); }
 
 #if 0
