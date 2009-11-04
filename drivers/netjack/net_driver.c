5b642bcabe470bd10c55ef93508aef88c5e47cf6
torben <torben@0c269be4-1314-0410-8aa9-9f06e86f4224>
2009-11-04 21:36:04 +0000

    update celt code to celt-0.7.0 api (fixes #139)
    git-svn-id: svn+ssh://jackaudio.org/trunk/jack@3690 0c269be4-1314-0410-8aa9-9f06e86f4224

--- drivers/netjack/net_driver.c
+++ drivers/netjack/net_driver.c
@@ -549,13 +549,13 @@ net_driver_attach (net_driver_t *driver)
 
 	if( driver->bitdepth == 1000 ) {
 #if HAVE_CELT
-	    celt_int32_t lookahead;
+	    celt_int32 lookahead;
 	    // XXX: memory leak
-	    CELTMode *celt_mode = celt_mode_create( driver->sample_rate, 1, driver->period_size, NULL );
+	    CELTMode *celt_mode = celt_mode_create( driver->sample_rate, driver->period_size, NULL );
 	    celt_mode_info( celt_mode, CELT_GET_LOOKAHEAD, &lookahead );
 	    driver->codec_latency = 2*lookahead;
 
-	    driver->capture_srcs = jack_slist_append(driver->capture_srcs, celt_decoder_create( celt_mode ) );
+	    driver->capture_srcs = jack_slist_append(driver->capture_srcs, celt_decoder_create( celt_mode, 1, NULL ) );
 #endif
 	} else {
 #if HAVE_SAMPLERATE 
@@ -597,8 +597,8 @@ net_driver_attach (net_driver_t *driver)
 	if( driver->bitdepth == 1000 ) {
 #if HAVE_CELT
 	    // XXX: memory leak
-	    CELTMode *celt_mode = celt_mode_create( driver->sample_rate, 1, driver->period_size, NULL );
-	    driver->playback_srcs = jack_slist_append(driver->playback_srcs, celt_encoder_create( celt_mode ) );
+	    CELTMode *celt_mode = celt_mode_create( driver->sample_rate, driver->period_size, NULL );
+	    driver->playback_srcs = jack_slist_append(driver->playback_srcs, celt_encoder_create( celt_mode, 1, NULL ) );
 #endif
 	} else {
 #if HAVE_SAMPLERATE
