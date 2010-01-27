2eb5de0a313aabd8f30856218ebee34acdc01a14
paul <paul@0c269be4-1314-0410-8aa9-9f06e86f4224>
2010-01-27 19:11:40 +0000

    correct spurious chars in last commit for jack.h
    git-svn-id: svn+ssh://jackaudio.org/trunk/jack@3881 0c269be4-1314-0410-8aa9-9f06e86f4224

--- jack/jack.h
+++ jack/jack.h
@@ -120,7 +120,7 @@ jack_client_t *jack_client_open (const char *client_name,
  *  NEW JACK CLIENTS</b>
  *
  */
-jack_client_t *jack_client_new (const char *client_name) JACK_OPTIONAL_WEAK_DEPRECATED_EXPORTdd;
+jack_client_t *jack_client_new (const char *client_name) JACK_OPTIONAL_WEAK_DEPRECATED_EXPORT;
 
 /**
  * Disconnects an external client from a JACK server.
