94af2e979669535f227ae2a913eaf473f416f4a2
torben <torben@0c269be4-1314-0410-8aa9-9f06e86f4224>
2011-05-29 00:47:49 +0000

    [python] add commandline prompt and switch command
    git-svn-id: svn+ssh://jackaudio.org/trunk/jack@4446 0c269be4-1314-0410-8aa9-9f06e86f4224

--- python/jackd.py
+++ python/jackd.py
@@ -3,6 +3,8 @@
 import sys
 from mygetopt import my_getopt
 import jackctl
+import readline
+
 
 argv = sys.argv[1:]
 
@@ -24,8 +26,6 @@ def server_parse_ags( srv, argv ):
 	    longopts.append (p.name)
 
 	shortmap[p.id] = p
-
-    print shortopts
 
     while not driver:
 	opts, argv = my_getopt( argv, shortopts+"d:" )
@@ -61,8 +61,6 @@ def driver_parse_args( drv, argv ):
 
 	shortmap[p.id] = p
 
-    print shortopts
-
     while True:
 	opts, argv = my_getopt( argv, shortopts+"d:" )
 	if not opts:
@@ -83,17 +81,36 @@ srv = jackctl.Server()
 drv, argv = server_parse_ags( srv, argv )
 driver_parse_args( drv, argv )
 
-
-for p in srv.params.values():
-    print p.name, "-> ", p.value
-
-print "----------------"
-print "driver ", drv.name
-
-for p in drv.params.values():
-    print p.name, "-> ", p.value
+#for p in srv.params.values():
+#    print p.name, "-> ", p.value
+#
+#print "----------------"
+#print "driver ", drv.name
+#
+#for p in drv.params.values():
+#    print p.name, "-> ", p.value
 
 srv.start( drv )
 
-while raw_input("hi> ") != "quit":
-    pass
+quit = False
+while not quit:
+    try:
+	cmd  = raw_input("jack> ")
+    except EOFError:
+	break
+
+    cmdv = cmd.split()
+
+    if len(cmdv) == 0:
+	continue
+
+    if cmdv[0] == "quit":
+	quit = True
+    elif cmdv[0] == "switch":
+	if len(cmdv) > 1:
+	    drv = srv.drivers[cmdv[1]]
+	    driver_parse_args( drv, cmdv[2:] )
+	    srv.switch_master( drv )
+
+print "\nshutting down"
+srv.stop()
